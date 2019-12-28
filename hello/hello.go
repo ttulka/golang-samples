343dd9cd15f92c7d0e520095b8eb60a1242c0521
Tomas Tulka <tomas.tulka@gmail.com>
2019-12-28 15:02:49 +0100

    hello world

--- hello/hello.go
+++ hello/hello.go
@@ -3,11 +3,5 @@ package main
 import "fmt"  
 
 func main() {
-	fmt.Printf("hello, world\n")
-  fmt.Printf(c()) // 2
-}
-
-func c() (i int) {
-    defer func() { i++ }()
-    return 1
-}
+	fmt.Printf("Hello, world!\n")  
+}	

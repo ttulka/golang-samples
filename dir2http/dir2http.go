7b1c2e244e5be4812893973030de54352867706f
Tomas Tulka <tomas.tulka@gmail.com>
2020-01-24 10:26:12 +0100

    streaming

--- dir2http/dir2http.go
+++ dir2http/dir2http.go
@@ -3,8 +3,8 @@ package main
 import (
   "net"
   "bufio"
-  //"io"
-  "io/ioutil"
+  "os"
+  "io"
   "strings"
   "log"
   "fmt"
@@ -17,6 +17,7 @@ func main() {
   }
   defer li.Close()
   
+  log.Println("Starting dir2http server...")
   for {
     conn, err := li.Accept()
     if err != nil {
@@ -32,17 +33,58 @@ func handle(conn net.Conn) {
     
   path := request(conn)
   
-  dat, err := ioutil.ReadFile(path)
-  if err != nil {
-    log.Println(err.Error())
+  if path == "" {
+    path = "index.html"
+  }
+  pathChanged := false
+  for strings.HasPrefix(path, "/") {
+    path = path[1:len(path)]
+    pathChanged = true
+  }
+  for strings.HasSuffix(path, "/") {
+    path = path[:len(path) - 1]
+    pathChanged = true
+  }  
+  if pathChanged {
+    redirection(conn, "/" + path)
+  }
+  
+  info, err := os.Stat(path)
+  if os.IsNotExist(err) {
     notfound(conn)
     return
   }
-             
+  if info.IsDir() {
+    path += "/index.html"
+  }
+  info, err = os.Stat(path)
+  if os.IsNotExist(err) {
+    forbidden(conn)
+    return
+  }
+  
+  f, err := os.Open(path)
+  if err != nil {
+    servererror(conn, err.Error())
+    return
+  }
+  
   fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
-  fmt.Fprintf(conn, "Content-Length: %d\r\n", len(dat))
+  fmt.Fprintf(conn, "Content-Length: %d\r\n", info.Size())
   fmt.Fprint(conn, "\r\n")
-  fmt.Fprint(conn, string(dat))
+  
+  for {
+    b := make([]byte, 1024)
+    n, err := f.Read(b)
+    if err != nil {
+      if err != io.EOF {
+        log.Println(err.Error())
+        return
+      }
+      break
+    }
+    fmt.Fprint(conn, string(b[:n]))
+  }
 }
 
 func request(conn net.Conn) string {
@@ -63,8 +105,28 @@ func request(conn net.Conn) string {
   return path
 }
 
+func redirection(conn net.Conn, loc string) {
+  fmt.Fprint(conn, "HTTP/1.1 302 Found\r\n")
+  fmt.Fprint(conn, "Content-Length: 0\r\n")
+  fmt.Fprintf(conn, "Location: %s\r\n", loc)
+  fmt.Fprint(conn, "\r\n")
+}
+
+func forbidden(conn net.Conn) {
+  fmt.Fprint(conn, "HTTP/1.1 403 Forbidden\r\n")
+  fmt.Fprint(conn, "Content-Length: 0\r\n")
+  fmt.Fprint(conn, "\r\n")
+}
+
 func notfound(conn net.Conn) {
   fmt.Fprint(conn, "HTTP/1.1 404 Not Found\r\n")
-  fmt.Fprintf(conn, "Content-Length: 0\r\n")
+  fmt.Fprint(conn, "Content-Length: 0\r\n")
   fmt.Fprint(conn, "\r\n")
+}
+
+func servererror(conn net.Conn, msg string) {
+  fmt.Fprint(conn, "HTTP/1.1 500 Internal Server Error\r\n")
+  fmt.Fprintf(conn, "Content-Length: %d\r\n", len(msg))
+  fmt.Fprint(conn, "\r\n")
+  fmt.Fprint(conn, msg)
 }

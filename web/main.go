ff1b1e6db7b8f4a6cdf527f97aadfc4763e35ddd
Tomas Tulka <tomas.tulka@gmail.com>
2020-01-30 15:53:17 +0100

    explicit server

--- web/main.go
+++ web/main.go
@@ -45,19 +45,20 @@ func page(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-  // Use the http.NewServeMux() function to initialize a new servemux, then
-  // register the home function as the handler for the "/" URL pattern.
   mux := http.NewServeMux()
+
   mux.HandleFunc("/", home)
   mux.HandleFunc("/page/", page)
-  
+
   mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static/"))))
   
-  // Use the http.ListenAndServe() function to start a new web server. We pass in
-  // two parameters: the TCP network address to listen on (in this case ":4000")
-  // and the servemux we just created. If http.ListenAndServe() returns an error
-  // we use the log.Fatal() function to log the error message and exit. Note
-  // that any error returned by http.ListenAndServe() is always non-nil.
+  server := &http.Server{
+    Addr: ":4000",
+    Handler: mux,
+  }
+  
   log.Println("Starting server on :4000")
-  log.Fatal(http.ListenAndServe(":4000", mux))
+  
+  err := server.ListenAndServe(":4000", mux)
+  log.Fatal(err)
 }
